c5c1221c6bc2cd5ece746b044a0099cb0025f3fa
Rajavamshee_123 <69705134+rajavamshee@users.noreply.github.com>
2023-02-08 22:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+function myFunc() {
+        	let name = document.getElementById("name").value;
+			let year = document.getElementById("year").value;
+        
+			if(name != "" && year != "") {
+				let newUrl = document.getElementById("url");
+				newUrl.innerText = newUrl.innerText + `?name=${name}&year=${year}`;
+			}
+			else if(name != "") {
+				let newUrl = document.getElementById("url");
+				newUrl.innerText = newUrl.innerText + `?name=${name}`;
+			}
+			else {
+				let newUrl = document.getElementById("url");
+				newUrl.innerText = newUrl.innerText + `?year=${year}`;
+			}
+}
